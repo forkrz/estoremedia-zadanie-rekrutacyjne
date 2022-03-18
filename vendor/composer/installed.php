2b7eec38b62aec215a2a6c54efa785313a63c8b9
forkrz <forkrz8@gmail.com>
2022-03-18 23:02:09 +0100

    x

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '33b1d002e5d09cbc355d91a0ad0e72a207b3329f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '33b1d002e5d09cbc355d91a0ad0e72a207b3329f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
